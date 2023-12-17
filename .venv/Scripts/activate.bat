60d3a7314906914f6af721903e47b14102e26209
Mahdi <106872357+MahdiAlimohammadlou@users.noreply.github.com>
2023-12-17 01:47:31 -0800

    add models

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Administrator\Desktop\cofe_project\.venv
+set VIRTUAL_ENV=E:\Maktab\GW\cafe_django\customer_branch\GW-django-cafe-g5\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
