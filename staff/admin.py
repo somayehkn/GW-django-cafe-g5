d7038a99cfd0276b66c617f4db1256a532e0d23f
hesam835 <hesamshadmehri@gmail.com>
2023-12-20 18:32:25 -0500

    register , migrate,set settin

--- staff/admin.py
+++ staff/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import User
+from .models import User,Staff
 
 admin.site.register(User)
+admin.site.register(Staff)
 # Register your models here.
--- staff/views.py
+++ staff/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+
 from django.core.files.storage import FileSystemStorage
 from customer.models import Category, Item
 import shutil
 from django.views.decorators.csrf import requires_csrf_token
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth import authenticate,login as django_login,logout as django_logout
+from django.contrib import messages
+from django.urls import reverse
+
 # Create your views here.
 @requires_csrf_token
 def add_items(request):
@@ -38,3 +44,28 @@ def add_items(request):
         new_item.save()
     context = Category.objects.all()
     return render(request, 'staff/add_item.html',{'categories':context})
+
+
+# create view for login
+def login(request):
+    if request.method == "GET":
+        form = AuthenticationForm()
+    elif request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]  
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                django_login(request,user)
+                messages.add_message(request,messages.SUCCESS,f"welcome{username}")
+                return redirect(reverse("dashboard"))
+            messages.add_message(request,messages.ERROR,f"user {username} was not found!")
+    return render(request,'login.html',context={"form":form})
+
+def dashboard(request):
+    return render(request,'dashboard.html',context={})
+    
+def logout(request):
+    django_logout(request)
+    return redirect(reverse("index")) 
