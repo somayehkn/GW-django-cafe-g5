a3d549fc470b2a6198d89ca9e7620c2f66c6005c
somayehkn <somayehkn102@gmail.com>
2023-12-27 13:40:52 +0330

    second commit

--- staff/forms.py
+++ staff/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from customer.models import Category
 
 class Form_Category(forms.ModelForm):
+    name = forms.CharField(label='نام دسته', widget=forms.TextInput(attrs={'placeholder': 'نام دسته را وارد کنید'}))
+    description = forms.CharField(label='توضیحات', widget=forms.Textarea(attrs={'placeholder': 'توضیحات را وارد کنید'}))
+    image = forms.ImageField(label='تصویر', widget=forms.FileInput(attrs={'placeholder': 'تصویر را انتخاب کنید'}))
+
     class Meta:
         model = Category
-        fields = "__all__" #["image","description","name_category"]
+        fields = ["name","description", "image"]
+
+    
