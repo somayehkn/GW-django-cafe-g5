930f294214659778197af7b191c82cb2602e7f4f
Alora-taken <63038255+Alora-taken@users.noreply.github.com>
2023-12-28 00:16:48 +0330

    add models

---
+++ staff/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+import django
+django.setup()
+from .models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="confirm password", widget=forms.PasswordInput)
+    
+    
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'full_name' , 'roll')
+    
+    
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password1"] and cd["password2"] and cd["password1"] != cd['password2']:
+            raise ValidationError("passwords dont match")
+        return cd["password2"]
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+    
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(help_text="you can change password using <a href=\"../password/\">this form.</a>")
+    
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'full_name', 'password', 'last_login' , 'roll')
+        
+        
+class UserRegister(forms.Form):
+    roll = forms.CharField(max_length = 11)
+    email = forms.EmailField()
+    full_name = forms.CharField(max_length = 50)
+    phone = forms.CharField(max_length = 50)
+    password = forms.CharField(max_length=10 , widget=forms.PasswordInput)
+    
+class VerifyCodeForm(forms.Form):
+    code = forms.IntegerField()
+    
+class LoginForm(forms.Form):
+    phone = forms.CharField(max_length=11)
+    password = forms.CharField(max_length=10,widget=forms.PasswordInput)
--- staff/models.py
+++ staff/models.py
@@ -1,58 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
-
-from django.db import models
-from django.contrib.auth.models import UserManager,AbstractBaseUser,PermissionsMixin
-
-
-# Create your models here.
-class CustumerUserManager(UserManager):
-    def _create_user(self , phone ,password,**extra_fields):
-        if not phone:
-            raise ValueError('User must have a phone')
-        
-        user = self.model(phone = phone)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_user(self,phone=None,password=None,**extra_fields):
-        extra_fields.setdefault('is_staff',True)
-        extra_fields.setdefault('is_superuser',False)
-        return self._create_user(phone,password,**extra_fields)
-            
-    def create_superuser(self,phone=None,password=None,**extra_fields):
-        extra_fields.setdefault('is_staff',True)
-        extra_fields.setdefault('is_superuser',True)
-        return self._create_user(phone,password,**extra_fields)
-    
-    
+# custom user
 class User(AbstractBaseUser,PermissionsMixin):
-    phone = models.CharField(max_length=12 ,default='', blank=True, unique=True)
-    name = models.CharField(max_length=100,blank=True)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
-    is_superuser = models.BooleanField(default = False)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField(max_length=255)
+    roll = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
     
-    USERNAME_FIELD = 'phone'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["email", "full_name","roll"]
     
-    objects=CustumerUserManager()
+    objects = UserManager()
     
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'User'
+    def __str__(self) -> str:
+        return self.email
     
-    def get_full_name(self):
-        return self.name
-
-
-class Staff(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20,null=True)
-    phone_number = models.CharField(max_length=20, unique=True)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+    def has_perm(self, perm, obj=None):
+        return True
     
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+class OTPCODE(models.Model):
+    phone_number = models.CharField(max_length=11)
+    code = models.PositiveSmallIntegerField()
+    created = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return f"{self.phone_number} - {self.code} - {self.created}"
+class staff_user(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    phone_number = models.CharField(max_length=11)
