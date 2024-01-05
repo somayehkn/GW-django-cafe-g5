797ae3dbb12bbd70aa96b85b6daa5e5193cbe871
somayehkn <somayehkn102@gmail.com>
2024-01-05 21:33:42 +0330

    create filterorders

--- customer/models.py
+++ customer/models.py
@@ -8,7 +8,7 @@ class Table(models.Model):
     capacity = models.IntegerField()
 
     def __str__(self) -> str:
-        return self.table_number
+        return str(self.table_number)
 
 
 class Customer(models.Model):
@@ -41,10 +41,19 @@ class Item(models.Model):
         return self.name
     
 class Customer_order(models.Model):
+    STATUS_CHOICES = [
+        ('Deliverd','Deliverd'),
+        ('Confirmed', 'Confirmed'),
+        ('Cooking', 'Cooking'),
+        ('Ready Delivery', 'Ready Delivery')
+     ]
+    
+    
     timestamp = models.DateTimeField()
     description = models.TextField(null=True)
     table_number = models.ForeignKey(Table, null=True, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     
     def save(self, *args, **kwargs):
         if not self.timestamp:
--- staff/urls.py
+++ staff/urls.py
@@ -1,26 +1,11 @@
 
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings  
 from django.conf.urls.static import static  
 from . import views
-from .views import TestView
+from .views import TestView, order_list_date, order_list_filter_status,  order_list_filter_table_number, order_list, order_detail
+
+
 urlpatterns = [
     path('add_item', views.add_items, name = 'add_item'),
     path("login/",views.login,name="login"),
@@ -31,8 +16,15 @@ urlpatterns = [
     path("table",views.table,name="table"),
     path("list_user",views.list_user.as_view(),name="list_user"),
     path("delete_user/<int:pk>/",views.DeleteUser.as_view(),name="delete_user"),
-    path("logout",views.logout,name="logout")
+    path("logout",views.logout,name="logout"),
+    path('status/', order_list, name='status'),
+    path('date/', order_list_date, name='date'),
+    path('filter-status/', order_list_filter_status, name='filter-status'),
+    path('filter-table/', order_list_filter_table_number, name='filter-table'),
+    path('order_detail/<int:order_id>/', order_detail, name='order_detail'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
+
+
